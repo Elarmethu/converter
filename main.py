f82d4f941fd84021b1f580ce2c2c3e99835852d8
Ignat Karelov-Suslov <elarmethu@gmail.com>
2022-06-24 01:32:59 +0300

    listener integration

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import imp
 from reader import Reader
+from listener import Listener
 from art import tprint
+import imp
 
 def logo_draw(): # draw logo project
-    tprint('PDF  TO   MP3') 
+    tprint('CONVERTER') 
 
 def main():
     logo_draw()
-    file_path = input('Enter file_path: \n')
-    print(Reader.read_file(file_path))
+    type_action = input("Enter what you want to do: 'listen' or 'record'. \n")
+    file_path = input('Enter the file with the fully specified path to it. \n')
+
+    if type_action == "'listen'" or type_action == 'listen':
+        print(Listener.write_file(file_path))
+    elif  type_action == "'record'" or type_action == 'record':
+        print(Reader.read_file(file_path))
 
 
 if (__name__ == '__main__'):
